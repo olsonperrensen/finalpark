ac2995b23d04f4f0c61e12323c777a82a07c4e65
olson <olson@dev.be>
2023-08-04 23:23:28 +0200

    removing parc working

--- VacationParcs.cpp
+++ VacationParcs.cpp
@@ -2,6 +2,7 @@
 // Created by Admin on 8/4/2023.
 //
 
+#include <algorithm>
 #include "VacationParcs.h"
 
 const std::string &VacationParcs::getVat() const {
@@ -40,6 +41,20 @@ void VacationParcs::addPark(Parcs park) {
     parcs.push_back(park);
 }
 
-void VacationParcs::removePark(int parkID) {
-//TODO combine with getParcId()
+void VacationParcs::removePark(int parkID)
+{
+    // Find the park to be removed using std::find_if and a lambda function
+    auto parkToRemove = std::find_if(parcs.begin(), parcs.end(), [&](const Parcs& p) {
+        return p.getParcId() == parkID;
+    });
+
+    // If the park was found, erase it from the vector
+    if (parkToRemove != parcs.end()) {
+        parcs.erase(parkToRemove);
+        std::cout << "Deleted park with ID " << parkID << std::endl;
+    } else {
+        std::cout << "Park with ID " << parkID << " not found." << std::endl;
+    }
 }
+
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "Parcs.h"
+#include "VacationParcs.h"
 #include <vector>
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
     LuxuryLevel l(false,false, false, false);
     std::vector<Accommodations> v;
     Accommodations a(1,13,2,false,l);
+    v.push_back(a);
     Parcs::ParcServices s(false,false, false, false, false, false);
+    std::vector<Parcs>pv;
     Parcs p("p","k",s,v);
-    std::cout << p.getParcId();
+    pv.push_back(p);
+    Customer c("lee","hong lane 1","bruce@lee.vn");
+    std::vector<Customer> cv;
+    cv.push_back(c);
+    VacationParcs vp("jonh", "lane 8", s,
+                     v, "BE01", pv,
+                     cv);
+
+    vp.removePark(1);
+
     return 0;
 }
