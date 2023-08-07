928767ba87aacb3d0090c7da2b2be5ca87c3f3c6
Iturria <Maximiliano.Iturria@sbdinc.com>
2023-08-07 14:44:41 +0200

    testing creating parcs ok

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main() {
     v.push_back(&a);
 
     Parcs::ParcServices s(true,true, true, true, true, true);
+    Parcs::ParcServices ss(false,false, true, true, false, true);
     std::vector<Parcs*>pv;
     Parcs* p = new Parcs("Destination 1","bloemlaan 7",s,v);
     pv.push_back(p);
@@ -45,43 +46,44 @@ int main() {
 //    vp.removePark(1);
 //    User test
     Owner o("Bruce","Kennisstraat 13","Lee@Lee.lee");
-    o.modifyPark(vp, 1, parkupdate);
-//    o.deletePark(vp,1);
-    o.createAccommodation(vp,2,anew);
-    o.modifyAccommodations(vp,2,vnew);
-//    o.deleteAccommodation(vp,2,999);
-    if(0){
-        int srv;
-        std::cout << "Choose which services to remove:" << R"(
-SUBTROPIC_SWIMMING_POOL=1,
-SPORTS_INFRASTRUCTURE=2,
-BOWLING_ALLEY=3,
-BICYCLE_RENT=4,
-CHILDRENS_PARADISE=5,
-WATER_BIKES=6)" << std::endl << "Choose: ";
-        std::cin >> srv;
-        o.changeService(vp,2,srv,false);
-    }
-//TODO only accept true w/static cast, not 1
-    Booking* b = new Booking(1,c,v,1,1,1,1);
-    Employee e("Tom Sawyer","laanbaan 1", "tom@boom.be", "Bancontact",false);
-    os.createCustomer(vp,neuc);
-    os.modifyCustomer(vp,2,Customer("Bush Anderson","Manhattanweg 33", "mht@work.cn", "Contant Geld"));
-
-    std::cout << "Customers present in VP:\n";
-    for (auto e:vp.getCustomers()) {
-//        TODO fix ID being replaced on all modify methods!
-        std::cout << std::endl << "Customer ID: " << e->getID() << std::endl;
-    }
-
-    os.bookAccommodation(vp,1,1,b);
-    os.modifyBooking(vp,1,Booking(72,neuc,v,0,1,0,1));
-//    e.deleteBooking(vp,72);
-//    e.deleteCustomer(vp,1);
-//individual modifying
-    os.modifyAccommodation(vp,2,1,afinal);
-    c->modifyData(Customer("Larry Page","Sergeistraat 9", "larry@goo.gl", "Bancontact"));
-
-    std::cout << vp;
+    o.createPark(vp, new Parcs("THE RESORT OF YOUR DREAMS!","Hollandsebaan 2",ss,vnew));
+//    o.modifyPark(vp, 1, parkupdate);
+////    o.deletePark(vp,1);
+//    o.createAccommodation(vp,2,anew);
+//    o.modifyAccommodations(vp,2,vnew);
+////    o.deleteAccommodation(vp,2,999);
+//    if(0){
+//        int srv;
+//        std::cout << "Choose which services to remove:" << R"(
+//SUBTROPIC_SWIMMING_POOL=1,
+//SPORTS_INFRASTRUCTURE=2,
+//BOWLING_ALLEY=3,
+//BICYCLE_RENT=4,
+//CHILDRENS_PARADISE=5,
+//WATER_BIKES=6)" << std::endl << "Choose: ";
+//        std::cin >> srv;
+//        o.changeService(vp,2,srv,false);
+//    }
+////TODO only accept true w/static cast, not 1
+//    Booking* b = new Booking(1,c,v,1,1,1,1);
+//    Employee e("Tom Sawyer","laanbaan 1", "tom@boom.be", "Bancontact",false);
+//    os.createCustomer(vp,neuc);
+//    os.modifyCustomer(vp,2,Customer("Bush Anderson","Manhattanweg 33", "mht@work.cn", "Contant Geld"));
+//
+//    std::cout << "Customers present in VP:\n";
+//    for (auto e:vp.getCustomers()) {
+////        TODO fix ID being replaced on all modify methods!
+//        std::cout << std::endl << "Customer ID: " << e->getID() << std::endl;
+//    }
+//
+//    os.bookAccommodation(vp,1,1,b);
+//    os.modifyBooking(vp,1,Booking(72,neuc,v,0,1,0,1));
+////    e.deleteBooking(vp,72);
+////    e.deleteCustomer(vp,1);
+////individual modifying
+//    os.modifyAccommodation(vp,2,1,afinal);
+//    c->modifyData(Customer("Larry Page","Sergeistraat 9", "larry@goo.gl", "Bancontact"));
+//
+//    std::cout << vp;
     return 0;
 }
