e14f4f63525178aa3a3a46fa2946da78861a340a
Iturria <Maximiliano.Iturria@sbdinc.com>
2023-08-07 15:56:52 +0200

    missing file

---
+++ Accommodations.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Admin on 8/4/2023.
+//
+
+#include "Accommodations.h"
+
+unsigned short int Accommodations::aantalAccommodations = 0;
+
+unsigned short int Accommodations::getID() const {
+    return accommodationID;
+}
+
+int Accommodations::getNrPeople() const {
+    return nrPeople;
+}
+
+void Accommodations::setNrPeople(int nrPeople) {
+    Accommodations::nrPeople = nrPeople;
+}
+
+int Accommodations::getSize() const {
+    return size;
+}
+
+void Accommodations::setSize(int size) {
+    Accommodations::size = size;
+}
+
+bool Accommodations::isBathroomWithBath() const {
+    return bathroomWithBath;
+}
+
+void Accommodations::setBathroomWithBath(bool bathroomWithBath) {
+    Accommodations::bathroomWithBath = bathroomWithBath;
+}
+
+const LuxuryLevel &Accommodations::getLuxuryLevel() const {
+    return luxuryLevel;
+}
+
+void Accommodations::setLuxuryLevel(const LuxuryLevel &luxuryLevel) {
+    Accommodations::luxuryLevel = luxuryLevel;
+}
+
+Accommodations::Accommodations(int nrPeople, int size, bool bathroomWithBath, LuxuryLevel luxuryLevel):
+       nrPeople(nrPeople),size(size),bathroomWithBath(bathroomWithBath),luxuryLevel(luxuryLevel){
+    this->accommodationID = ++Accommodations::aantalAccommodations;
+}
